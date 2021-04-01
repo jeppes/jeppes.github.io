37fc8f4b96946dc870b6f16fadfb332789a7d437
Jesper <jesper@sanalabs.com>
2021-04-01 22:45:29 +0200

    Remove react-syntax-highlighter

--- src/main/jeppes/frontend/markdown.cljs
+++ src/main/jeppes/frontend/markdown.cljs
@@ -1,14 +1,6 @@
 (ns jeppes.frontend.markdown
   (:require
-   [reagent.core :as r]
-   ["react-markdown" :as ReactMarkdown]
-   ["react-syntax-highlighter" :refer [PrismAsyncLight]]
-   ["react-syntax-highlighter/dist/esm/styles/prism" :refer [darcula]]))
-
-(defn- highlight [^js args]
-  (r/create-element PrismAsyncLight #js{:style darcula
-                                        :children (.-value args)
-                                        :language (.-language args)}))
+   ["react-markdown" :as ReactMarkdown]))
 
 (defn markdown [text]
-  [:> ReactMarkdown {:renderers {:code highlight}} text])
+  [:> ReactMarkdown text])
