3cc184648a2e1eff2fa5b7d2942861610bd0f7dc
Jesper <jespersandstrom@gmail.com>
2026-07-28 22:51:52 +0200

    Port to plain HTML

--- src/main/jeppes/frontend/pages/notfound.cljs
+++
@@ -1,7 +0,0 @@
-(ns jeppes.frontend.pages.notfound
-  (:require [jeppes.frontend.router :refer [link]]))
-
-(defn not-found []
-  [:<>
-   [:p "404 - Page not found"]
-   [:p "Take me " [link [""] "home"] "."]])
